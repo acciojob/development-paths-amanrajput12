424be0fc3e23947f82b189925686dd71279fb57a
Aman Rajput <89723728+amanrajput12@users.noreply.github.com>
2025-02-21 17:06:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+let myval = " Development";
+// console.log(value)
+
+const data = document.querySelectorAll("li")
+
+
+	data.forEach(value=>value.textContent+= myval)
+
